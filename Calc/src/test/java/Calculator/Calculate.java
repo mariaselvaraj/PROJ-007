04a2dfa4d7ecb417695c61a48e0e39952166fc91
Maria Selvaraj <sj.marria@gmail.com>
2016-05-05 23:34:27 +0530

    first commit

---
+++ Calc/src/test/java/Calculator/Calculate.java
@@ -0,0 +1,28 @@
+package Calculator;
+
+import org.testng.annotations.Test;
+
+public class Calculate {
+	
+	@Test
+	public void Add(){
+		System.out.println("Addition successfull");
+	}
+	
+	@Test
+	public void Sub(){
+		System.out.println("Subtraction successfull");
+	}
+	
+	@Test
+	public void Mul(){
+		System.out.println("Multiplication successfull");
+	}
+	
+	@Test
+	public void Div(){
+		System.out.println("Division successfull");
+	}
+	
+
+}
